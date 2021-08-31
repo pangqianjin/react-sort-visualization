2fe4842e2bc59dd00ff5d3dffd7ea742dd8eb7a8
pangqianjin <2594622441@qq.com>
2021-08-31 11:48:16 +0800

    add bubble sort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function* bubbleSort(arr) {
+  let tmp;
+  for (let i = arr.length; i > 0; i--) {// 较大的arr[j]会冒泡到arr的尾部
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {// 前一个元素比或一个大，则向后冒泡(交换)
+        tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+        yield arr
+      }
+    }
+  }
 }
 
-export default App;
+export default class App extends Component {
+  state = {
+    arr: [1, 3, 5, 2, 6, 10, 7, 5, 9],
+    delay: 1// second
+  }
+
+  startSort = () => {
+    const { arr } = this.state
+    const arrGen = bubbleSort(arr)
+    let newArr = [...arr]
+    while (newArr) {
+      this.setState({arr: newArr})
+      newArr = arrGen.next().value
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.startSort}>▶</button>
+        {
+          this.state.arr.map((item, index) => (
+            <div className='cylinder' style={{ top: `${20 - item}rem`, height: `${item}rem` }} key={index}>
+              {item}
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+}
